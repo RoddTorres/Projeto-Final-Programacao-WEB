078c9aabdc2b367242f920230cfdebbec6d1752a
Rodrigo Torres de Souza <rtsouza87@gmail.com>
2022-07-18 18:17:31 -0300

    Novo projeto

---
+++ backend/src/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+      name: "Ernie Ball 2835 Extra Slinky Bass Nickel Wound (40-95)",
+      imageUrl:
+        "https://www.stringsbymail.com/images/P02835.jpg",
+      description:
+        "Ernie Ball Extra Slinky Bass Guitar Strings are round-wound for maximum clarity and serious punch. Each bass string is manufactured with the finest and freshest raw materials in the beautiful Coachella Valley of Southern California. The list of endorsers for Ernie Ball Slinky bass strings reads like a who's who of top selling artists.",
+      price: 20,
+      countInStock: 12,
+    },
+    {
+      name: "Ernie Ball 2836 Regular Slinky 5-String Bass Nickel Wound, 45-130",
+      imageUrl:
+        "https://www.stringsbymail.com/images/P02836.jpg",
+      description:
+        "Ernie Ball Nickel Wound Electric Bass Strings are made from nickel plated steel wrapped around a hex shaped steel core wire. Each bass guitar string produces a bright, balanced tone and is manufactured with the finest and freshest raw materials in the beautiful Coachella Valley of Southern California.",
+      price: 25,
+      countInStock: 19,
+    },
+    {
+      name: "Ernie Ball 2838 6-string Long Scale Slinky Bass Nickel Wound, 32-130",
+      imageUrl:
+        "https://www.stringsbymail.com/images/P02838.jpg",
+      description:
+        "Optimal for basses with a longer scale, taper begins at 37 1/4\" Ernie Ball 6-string bass string sets are round-wound for maximum clarity and serious punch. Each bass string is manufactured with the finest and freshest raw materials in the beautiful Coachella Valley of Southern California. The list of endorsers for Ernie Ball Slinky bass strings reads like a who's who of top selling artists.",
+      price: 33,
+      countInStock: 7,
+    },
+    {
+      name: "Fender 7250L Nickel-Plated Steel Roundwound Bass Strings (40-100)",
+      imageUrl:
+        "https://www.stringsbymail.com/images/Fender-7250L.jpg",
+      description:
+        "Fender 7250 nickel-plated steel strings combine the high output and dynamic sound of steel with the smooth feel of nickel. These roundwound strings are bright and punchy and are perfect for rock, blues and more. There's no better way to let your bass personality shine through than with this highly articulate string set.",
+      price: 20,
+      countInStock: 14,
+    },
+    {
+      name: "Fender 7250M Nickel-Plated Steel Roundwound Bass Strings (45-105)",
+      imageUrl:
+        "https://www.stringsbymail.com/images/Fender-7250M.jpg",
+      description:
+        "Fender 7250 nickel-plated steel strings combine the high output and dynamic sound of steel with the smooth feel of nickel. These roundwound strings are bright and punchy and are perfect for rock, blues and more. There's no better way to let your bass personality shine through than with this highly articulate string set.",
+      price: 20,
+      countInStock: 20,
+    },
+    {
+      name: "Fender 9050M Stainless Steel Flatwound Electric Bass Strings (55-105)",
+      imageUrl:
+        "https://www.stringsbymail.com/images/Fender%209050M.jpg",
+      description:
+        "Fender stainless 9050 flatwound strings offer warm, rich tone with reduced finger noise and a long life. For reggae, jazz, country, R&B and more, there's no better way to sustain mellow flatwound tone with a sleek feel and ultimate tuning stability.",
+      price: 35,
+      countInStock: 18,
+    },
+  ];
+  
+  module.exports = products;
